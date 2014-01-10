3cc31410de2240932a9f2fcebccd12477ec44860
bharath kumar4 <bharath_kumar4@dell.com>
2014-01-10 11:07:52 +0000

    changes for ioa Resource type, Spec test cases for Dell  Interface, portchannel, vlan

--- spec/unit/puppet/type/mxl_vlan_spec.rb
+++ spec/unit/puppet/type/mxl_vlan_spec.rb
@@ -12,7 +12,9 @@ describe Puppet::Type.type(:mxl_vlan) do
   let :resource do
     described_class.new(
         	:name => '145',
-  		:shutdown    => true
+  		:shutdown    => true,
+                :vlan_name => 'SomeVlan',
+                :mtu => '1200' 
 	)
   end
 
@@ -48,6 +50,36 @@ describe Puppet::Type.type(:mxl_vlan) do
     describe "for name" do
       it "should allow valid vlan" do
         resource.name.should eq( '145')
+      end
+    end
+
+    describe "for shutdown" do
+      it "should allow a valid shutdown" do
+        described_class.new(:name => resource.name, :shutdown => :true)[:shutdown].should == :true
+      end
+    end
+
+    describe "for shutdown" do
+      it "should not allow a invalid shutdown" do
+        expect { described_class.new(:name => resource.name, :shutdown => '100')[:shutdown].should == '100' }.to raise_error
+      end
+    end
+
+    describe "for vlan_name" do
+      it "should allow a valid name" do
+        described_class.new(:name => resource.name, :vlan_name => 'somename')[:vlan_name].should == 'somename'
+      end
+    end
+
+    describe "for mtu" do
+      it "should allow a valid mtu" do
+        described_class.new(:name => resource.name, :mtu => '1900')[:mtu].should == '1900'
+      end
+    end
+
+    describe "for mtu negative test case" do
+      it "should not allow a invalid mtu" do
+        expect { described_class.new(:name => resource.name, :mtu => 'abc')[:mtu].should == 'abc' }.to raise_error
       end
     end
 
