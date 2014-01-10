eb00ab28c07dc9aa2bbbe548fac0da9f31947c5e
umesha bhat <umesha_bhat@dell.com>
2014-01-10 09:56:27 +0000

    Rspec changes for dell_iom portchannel  type

--- spec/unit/puppet/type/mxl_portchannel_spec.rb
+++ spec/unit/puppet/type/mxl_portchannel_spec.rb
@@ -2,25 +2,19 @@
 
 require 'spec_helper'
 
-
-
 describe Puppet::Type.type(:mxl_portchannel) do
-
-
-
 
   let :resource do
     described_class.new(
-        	:name => '110',
-  		:shutdown    => true
-	)
+    :name => '110',
+    :desc=>'test',
+    :shutdown    => true
+    )
   end
 
   it "should have a 'name' parameter'" do
     described_class.new(:name => resource.name)[:name].should == '110'
   end
-
-
 
   describe "when validating attributes" do
     [ :name ].each do |param|
@@ -29,15 +23,12 @@ describe Puppet::Type.type(:mxl_portchannel) do
       end
     end
 
-    [ :shutdown ].each do |property|
+    [ :shutdown , :mtu, :desc ].each do |property|
       it "should have a #{property} property" do
         described_class.attrtype(property).should == :property
       end
     end
   end
-
-
-
 
   describe "when validating attribute values" do
     before do
@@ -51,14 +42,40 @@ describe Puppet::Type.type(:mxl_portchannel) do
       end
     end
 
+    describe "for desc" do
+      it "should allow a valid desc " do
+        described_class.new(:name => resource.name, :desc => 'testdesc')[:desc].should == 'testdesc'
+      end
+    end
 
+    describe "for mtu " do
+      it "should allow a valid mtu" do
+        described_class.new(:name => resource.name, :mtu => '600')[:mtu].should == '600'
+      end
+    end
+    describe 'for mtu  invalid input ' do
 
+      it "should raise an exception on everything else" do
+        expect { described_class.new(:name => resource.name, :mtu => 'xyz') }.to raise_error
+        expect { described_class.new(:name => resource.name, :mtu => '500') }.to raise_error
+        expect { described_class.new(:name => resource.name, :mtu=> '13000') }.to raise_error
 
- end
+      end
+    end
 
+    describe 'for shutdown' do
+      [ :true, :false ].each do |val|
+        it "should allow the value #{val.inspect}" do
+          described_class.new(:name => resource.name, :shutdown => val)
+        end
+      end
 
-  
+      it "should raise an exception on everything else" do
+        expect { described_class.new(:name => resource.name, :shutdown => :foobar) }.to raise_error
+      end
+    end
+
+  end
+
 end
-
-
 
