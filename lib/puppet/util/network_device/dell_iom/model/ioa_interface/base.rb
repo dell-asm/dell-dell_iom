f3425c3a433c684343ba20b827235707e94678e7
bharath kumar4 <bharath_kumar4@dell.com>
2014-01-16 10:25:34 +0000

    changes for ioa Resource type, (Included the noop changes)

--- lib/puppet/util/network_device/dell_iom/model/ioa_interface/base.rb
+++ lib/puppet/util/network_device/dell_iom/model/ioa_interface/base.rb
@@ -3,8 +3,10 @@ require 'puppet/util/network_device/dell_iom/model/ioa_interface'
 
 module Puppet::Util::NetworkDevice::Dell_iom::Model::Ioa_interface::Base
   def self.ifprop(base, param, base_command = param, &block)
-    base.register_scoped param, /^(interface\s+(\S+).*?)^!/m do
-      cmd 'sh interface status'
+    interfaceval = base.name
+
+    base.register_scoped param, /(^Name: (\S*\s+\S*).*^\s+)/m do
+      cmd "show interfaces switchport #{interfaceval}"
       match /^\s*#{base_command}\s+(.*?)\s*$/
       add do |transport, value|
         Puppet.debug(" command #{base_command} value  #{value}" )
@@ -19,23 +21,29 @@ module Puppet::Util::NetworkDevice::Dell_iom::Model::Ioa_interface::Base
   end
 
   def self.register(base)
+    interfaceval = base.name
     txt = ''
     ifprop(base, :ensure) do
       match do |txt|
-        unless txt.nil?
-          txt.match(/\S+/) ? :present : :absent
-        else
-          :absent
-        end
+      	:present
       end
-      default :absent
+      default :present
       add { |*_| }
       remove { |*_| }
     end
 
     ifprop(base, :vlan_tagged) do
-      match /^\s*vlan tagged\s+(.*?)\s*$/
+      match do |txt|
+        paramsarray=txt.match(/^T\s+(\S+)/)
+        if paramsarray.nil?
+           param1 = :absent
+        else
+           param1 = paramsarray[1]
+        end
+        param1
+      end
       add do |transport, value|
+        transport.command("no vlan tagged 1-4094")
         transport.command("vlan tagged #{value}")
       end
       remove do |transport, old_value|
@@ -44,17 +52,39 @@ module Puppet::Util::NetworkDevice::Dell_iom::Model::Ioa_interface::Base
     end
 
     ifprop(base, :vlan_untagged) do
-      match /^\s*vlan untagged\s+(.*?)\s*$/
+      match  do |txt|
+        paramsarray=txt.match(/^U\s+(\S+)/)
+        if paramsarray.nil?
+           param1 = :absent
+        else
+           param1 = paramsarray[1]
+        end
+        param1
+      end
+
       add do |transport, value|
+        transport.command("no vlan untagged 1-4094")
         transport.command("vlan untagged #{value}")
       end
       remove do |transport, old_value|
         transport.command("no vlan untagged #{old_value}")
       end
     end
+     
+    general_scope = /^((#{interfaceval}).*)/m
 
-    ifprop(base, :shutdown) do
-      match /^\s*shutdown\s+(.*?)\s*$/
+    base.register_scoped :shutdown, general_scope do
+      match do |txt|
+          paramsarray=txt.match(/^#{interfaceval} is up/)
+          if paramsarray.nil?
+            param1 = :true
+          else
+            param1 = :false
+          end
+          param1
+      end
+
+      cmd "show interfaces #{interfaceval}"
       add do |transport, value|
         if value==:true
           transport.command("shutdown")
