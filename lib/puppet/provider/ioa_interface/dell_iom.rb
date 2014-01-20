3356a57ddd133b7ad892af5dc4fbe09c21ccd386
Bharath Kumar <bharath_kumar4@dell.com>
2014-01-20 15:09:12 +0530

    Fixed the NOOP issue with MXL_Interface and IOA_interface.

--- lib/puppet/provider/ioa_interface/dell_iom.rb
+++ lib/puppet/provider/ioa_interface/dell_iom.rb
@@ -3,17 +3,27 @@
 require 'puppet/provider/dell_iom'
 
 Puppet::Type.type(:ioa_interface).provide :dell_iom, :parent => Puppet::Provider::Dell_iom do
-  desc "Dell IOA provider for interface configuration."
+  desc "force 10 IOA Switch Interface Provider for Device Configuration."
   mk_resource_methods
   def initialize(device, *args)
     super
   end
 
   def self.lookup(device, name)  
+    
+
+    if !name.nil?
+      name=name.gsub(/te |tengigabitethernet /i, "TenGigabitEthernet ")
+      name=name.gsub(/fo |fortygige /i, "fortyGigE ")
+    end
+
+    paramstohash = device.switch.ioa_interface(name).params_to_hash
     device.switch.ioa_interface(name).params_to_hash
   end
 
   def flush
+
+
     device.switch.ioa_interface(name).update(former_properties, properties)
     super
   end
--- lib/puppet/provider/mxl_interface/dell_iom.rb
+++ lib/puppet/provider/mxl_interface/dell_iom.rb
@@ -3,13 +3,19 @@
 require 'puppet/provider/dell_iom'
 
 Puppet::Type.type(:mxl_interface).provide :dell_iom, :parent => Puppet::Provider::Dell_iom do
-  desc "Dell MXL switch provider for interface configuration."
+  desc "force 10 Switch Interface Provider for Device Configuration."
   mk_resource_methods
   def initialize(device, *args)
     super
   end
 
   def self.lookup(device, name)
+
+    if !name.nil?
+      name=name.gsub(/te |tengigabitethernet /i, "TenGigabitEthernet ")
+      name=name.gsub(/fo |fortygige /i, "fortyGigE ")
+    end
+
     device.switch.interface(name).params_to_hash
   end
 
