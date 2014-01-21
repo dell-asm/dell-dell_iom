9a96e873f1804907e5f7a90b77dd0b7a7cc8261d
uvbhat11 <umesha_bhat@dell.com>
2014-01-21 10:31:35 +0530

    RSpec integration for MXL and IOA. Formatted and removed commented code

--- spec/integration/puppet/provider/ioa_interface/dell_iom_spec.rb
+++ spec/integration/puppet/provider/ioa_interface/dell_iom_spec.rb
@@ -4,16 +4,15 @@ require 'spec_helper'
 require 'puppet/util/network_device/dell_iom/device'
 require 'puppet/provider/ioa_interface/dell_iom'
 
-
 describe "Integration test for IOA Interface" do
 
-  #device_conf =  YAML.load_file(my_deviceurl('dell_powerconnect','device_conf.yml'))    
+  #device_conf =  YAML.load_file(my_deviceurl('dell_powerconnect','device_conf.yml'))
   provider_class = Puppet::Type.type(:ioa_interface).provider(:dell_iom)
 
   before do
     #Facter.stub(:value).with(:url).and_return(device_conf['url'])
-    @device = provider_class.device("ssh://root:calvin@172.152.0.80")   
-  end  
+    @device = provider_class.device("ssh://root:calvin@172.152.0.80")
+  end
 
   let :config_ioa_interface do
     Puppet::Type.type(:ioa_interface).new(
@@ -24,14 +23,11 @@ describe "Integration test for IOA Interface" do
     )
   end
 
-
-
-  context 'when configuring ioa interface' do 
-    it "should configure ioa interface" do 
+  context 'when configuring ioa interface' do
+    it "should configure ioa interface" do
       preresult = provider_class.lookup(@device, config_ioa_interface[:name])
 
       @device.switch.ioa_interface(config_ioa_interface[:name]).update(preresult,{:vlan_tagged => config_ioa_interface[:vlan_tagged], :vlan_untagged => config_ioa_interface[:vlan_untagged], :shutdown =>config_ioa_interface[:shutdown]})
-
 
       postresult = provider_class.lookup(@device, config_ioa_interface[:name])
       postresult.should eq({:ensure => :present, :vlan_tagged => config_ioa_interface[:vlan_tagged], :vlan_untagged => config_ioa_interface[:vlan_untagged], :shutdown => config_ioa_interface[:shutdown]})
@@ -39,5 +35,4 @@ describe "Integration test for IOA Interface" do
   end
 
 end
-
 
