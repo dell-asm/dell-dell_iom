511e8ed17ba018c8a50b0080a7426e3a85c0f6d0
Sushil Rai <Sushil_R@Dell.com>
2014-09-12 19:28:02 +0530

    Adding support for quadmode configuration for IOA and MXL ports
    This will enable / disable the quad mode on the interface.

---
+++ lib/puppet/provider/mxl_quadmode/dell_iom.rb
@@ -0,0 +1,25 @@
+#Provide for MXL 'Interface' Type for managing the quad port
+
+require 'puppet/provider/dell_iom'
+
+Puppet::Type.type(:mxl_quadmode).provide :dell_iom, :parent => Puppet::Provider::Dell_iom do
+  desc "Dell MXL switch provider for interface quadmode configuration."
+  mk_resource_methods
+  def initialize(device, *args)
+    super
+  end
+
+  def self.lookup(device, name)
+    if !name.nil?
+      name=name.gsub(/te |tengigabitethernet /i, "TenGigabitEthernet ")
+      name=name.gsub(/fo |fortygige /i, "fortyGigE ")
+      name=name.gsub(/fc\s*/i, "fibreChannel ")
+    end
+    device.switch.quadmode(name).params_to_hash
+  end
+
+  def flush
+    device.switch.quadmode(name).update(former_properties, properties)
+    super
+  end
+end
